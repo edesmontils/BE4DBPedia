fb9fb9846bc7ebcf7e6de0daff00b650f287a8ea
Patricia Serrano <patricia.serrano@univ-nantes.fr>
2017-05-12 11:15:37 +0200

    Added queries

--- queries.sql
+++ queries.sql
@@ -3,27 +3,32 @@
 -- Execute this script with source queries.sql
 
 -- Queries and their results are spool on queries_result.lst
+SYSTEM rm queries.lst
 tee queries.lst 
 
 -- This table contains all values of precision and recall calculated as a cartesian product of ground truth queries (SPARQL queries of DBpedia) and deduced queries (BGP's extraction of LIFT)
 DESC precisionRecall;
 
--- Returns the number of BGPs 
-SELECT COUNT(*) 
-FROM (SELECT bgp_dbp 
-	FROM precisionRecall 
-	GROUP BY date_,ip,bgp_dbp) x
-;
+-- This table contains the highgest values of the "quality" of LIFT deductions. We consider as quality the average of precision and recall.
+DROP TABLE QUALITY
+CREATE TABLE QUALITY
+(
+date_ varchar(20),
+ip varchar(33),
+bgp_dbp varchar(2000),
+nb_occurrences_dbp int(4),
+rank_dbp int(5),
+quality DECIMAL(6,5))
+	SELECT date_,ip,bgp_dbp,nb_occurrences_dbp,rank_dbp, MAX((preci+recall)/2) quality
+	FROM precisionRecall
+	GROUP BY date_,ip,bgp_dbp,nb_occurrences_dbp,rank_dbp;
 
--- Returns the average between precision and recall
+-- Returns the average of the quality of the LIFT deductions
 SELECT AVG(quality) 
-FROM (SELECT date_,ip, MAX((preci+recall)/2) quality 
-	FROM precisionRecall 
-	GROUP BY date_,ip,bgp_dbp) x
-;
+FROM quality;
 
 -- This table contains only the highest values of precision. For time performance reason this table will be preferable used in next queries.
-DROP TABLE preci;
+DROP TABLE PRECI;
 CREATE TABLE preci(
 date_ varchar(20),
 ip varchar(33),
@@ -62,12 +67,17 @@ FROM preci
 GROUP BY precision_
 ORDER BY 1 DESC;
 
-
 -- Returns the different values of recall and the number of BGPs having that value
 SELECT count(recall) nb_bgps_date_ip, recall
 FROM recall
 GROUP BY recall
 ORDER BY 1 DESC;
+
+-- Returns the average of precision obtained by date 
+SELECT date_, AVG(quality)
+FROM quality
+GROUP BY date_
+ORDER BY 2 DESC;
 
 -- Returns the average of precision obtained by date 
 SELECT date_, AVG(precision_)
@@ -82,17 +92,30 @@ GROUP BY date_
 ORDER BY 2 DESC;
 
 -- Returns the average of precision by ip
+SELECT ip, AVG(quality) 
+FROM quality
+GROUP BY ip  
+ORDER BY 2 DESC;
+
+-- Returns the average of precision by ip
 SELECT ip, AVG(precision_) 
 FROM preci
 GROUP BY ip  
 ORDER BY 2 DESC;
-
 
 -- Returns the average of recall by ip
 SELECT ip, AVG(recall) 
 FROM recall
 GROUP BY ip 
 ORDER BY 2 DESC;
+
+-- Returns the global quality of queries that occurred more than once
+SELECT AVG(avg_quality_repet)
+FROM (SELECT COUNT(*), AVG(quality) avg_quality_repet
+	FROM quality
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1) y
+;
 
 -- Returns the global precision of queries that occurred more than once
 SELECT AVG(avg_preci_repet)
@@ -110,8 +133,67 @@ FROM (SELECT COUNT(*), AVG(recall) avg_recall_repet
 	HAVING count(*)>1) y
 ;
 
+==
+-- Returns the quality of queries that occurred more than once
+SELECT COUNT(*), AVG(quality) avg_quality_repet
+FROM quality
+GROUP BY bgp_dbp 
+HAVING count(*)>1
+ORDER BY 1 DESC;
 
--- Next queries return the number of occurrences (if higher than 1) of the TOP N most repeated BGPs
+-- Returns the precision of queries that occurred more than once
+SELECT COUNT(*), AVG(precision_) avg_preci_repet
+FROM preci
+GROUP BY bgp_dbp 
+HAVING count(*)>1
+ORDER BY 1 DESC;
+
+-- Returns the recall of queries that occurred more than once
+SELECT COUNT(*), AVG(recall) avg_recall_repet
+FROM recall
+GROUP BY bgp_dbp 
+HAVING count(*)>1
+ORDER BY 1 DESC;
+
+==
+-- Next queries return the number of occurrences (if higher than 1) and the quality of the TOP N most repeated BGPs
+SELECT AVG(avg_quality_repet) 
+FROM (SELECT COUNT(*), AVG(quality) avg_quality_repet 
+	FROM quality 
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1 
+	ORDER BY 1 DESC
+	LIMIT 20) x
+;
+
+SELECT AVG(avg_quality_repet) 
+FROM (SELECT COUNT(*), AVG(quality) avg_quality_repet 
+	FROM quality 
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1 
+	ORDER BY 1 DESC
+	LIMIT 15) x
+;
+
+SELECT AVG(avg_quality_repet) 
+FROM (SELECT COUNT(*), AVG(quality) avg_quality_repet 
+	FROM quality 
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1 
+	ORDER BY 1 DESC
+	LIMIT 10) x
+;
+
+SELECT AVG(avg_quality_repet) 
+FROM (SELECT COUNT(*), AVG(quality) avg_quality_repet 
+	FROM quality 
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1 
+	ORDER BY 1 DESC
+	LIMIT 5) x
+;
+
+-- Next queries return the number of occurrences (if higher than 1) and the precision of the TOP N most repeated BGPs
 SELECT AVG(avg_preci_repet) 
 FROM (SELECT COUNT(*), AVG(precision_) avg_preci_repet 
 	FROM preci 
@@ -127,7 +209,7 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_preci_repet
 	GROUP BY bgp_dbp 
 	HAVING count(*)>1 
 	ORDER BY 1 DESC
-	LIMIT 10) x
+	LIMIT 15) x
 ;
 
 SELECT AVG(avg_preci_repet) 
@@ -136,7 +218,7 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_preci_repet
 	GROUP BY bgp_dbp 
 	HAVING count(*)>1 
 	ORDER BY 1 DESC
-	LIMIT 15) x
+	LIMIT 10) x
 ;
 
 SELECT AVG(avg_preci_repet) 
@@ -148,9 +230,9 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_preci_repet
 	LIMIT 5) x
 ;
 
--- Next queries return the number of occurrences (if higher than 1) of the TOP N most repeated BGPs
+-- Next queries return the number of occurrences (if higher than 1) and the recall of the TOP N most repeated BGPs
 SELECT AVG(avg_recall_repet) 
-FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet 
+FROM (SELECT COUNT(*), AVG(recall) avg_recall_repet 
 	FROM recall 
 	GROUP BY bgp_dbp 
 	HAVING count(*)>1 
@@ -159,16 +241,7 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet
 ;
 
 SELECT AVG(avg_recall_repet) 
-FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet 
-	FROM recall 
-	GROUP BY bgp_dbp 
-	HAVING count(*)>1 
-	ORDER BY 1 DESC
-	LIMIT 15) x
-;
-
-SELECT AVG(avg_recall_repet) 
-FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet 
+FROM (SELECT COUNT(*), AVG(recall) avg_recall_repet 
 	FROM recall 
 	GROUP BY bgp_dbp 
 	HAVING count(*)>1 
@@ -177,7 +250,16 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet
 ;
 
 SELECT AVG(avg_recall_repet) 
-FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet 
+FROM (SELECT COUNT(*), AVG(recall) avg_recall_repet 
+	FROM recall 
+	GROUP BY bgp_dbp 
+	HAVING count(*)>1 
+	ORDER BY 1 DESC
+	LIMIT 15) x
+;
+
+SELECT AVG(avg_recall_repet) 
+FROM (SELECT COUNT(*), AVG(recall) avg_recall_repet 
 	FROM recall 
 	GROUP BY bgp_dbp 
 	HAVING count(*)>1 
@@ -187,19 +269,20 @@ FROM (SELECT COUNT(*), AVG(precision_) avg_recall_repet
 
 
 -- nb_occurrences_dbp gives the number of same queries existing in one hour in the ground truth. As LIFT merges these same queries if they appear in the same input log, only one of them was executed and analyzed with lift.
--- Returns by ip the number of occurrence of the same BGP and their precision
-SELECT ip,sum(nb_occurrences_dbp), AVG(precision_) 
-FROM (SELECT date_,ip, nb_occurrences_dbp, MAX(preci) precision_
-	FROM precisionRecall 
-	GROUP BY date_,ip,bgp_dbp,nb_occurrences_dbp) x 
+
+-- Returns by ip the number of occurrence of the same BGP and their quality
+SELECT ip,sum(nb_occurrences_dbp), AVG(quality) 
+FROM quality
 group by ip ORDER BY 2 DESC;
 
--- nb_occurrences_dbp gives the number of same queries existing in one hour in the ground truth. As LIFT merges these same queries if they appear in the same input log, only one of them was executed and analyzed with lift.
+-- Returns by ip the number of occurrence of the same BGP and their precision
+SELECT ip,sum(nb_occurrences_dbp), AVG(precision_) 
+FROM preci
+group by ip ORDER BY 2 DESC;
+
 -- Returns by ip the number of occurrence of the same BGP and their recall
 SELECT ip, SUM(nb_occurrences_dbp), AVG(recall) 
-FROM (SELECT date_,ip, nb_occurrences_dbp, MAX(recall) recall
-	FROM precisionRecall 
-	GROUP BY date_,ip,bgp_dbp,nb_occurrences_dbp) x 
+FROM recall 
 group by ip 
 ORDER BY 2 DESC;
 
